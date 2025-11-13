1001c8240c504bea4732e02e63a67ca86b22a98d
ItsFlicker <1812496278@qq.com>
2025-11-13 16:35:10 +0800

    [2.3.15] 蠢了

--- project/runtime-bukkit/src/main/kotlin/me/arasple/mc/trchat/module/internal/TrChatBukkit.kt
+++ project/runtime-bukkit/src/main/kotlin/me/arasple/mc/trchat/module/internal/TrChatBukkit.kt
@@ -51,7 +51,6 @@ object TrChatBukkit : Plugin() {
 //        registerLifeCycleTask(LifeCycle.INIT, 0) {
 //            YamlUpdater.update("settings.yml", updateExists = false)
 //        }
-        MinecraftVersion.supportedVersion[13] = arrayOf("!1.21", "1.21.1", "!1.21.2", "1.21.3", "1.21.4", "1.21.5", "!1.21.6", "1.21.7", "1.21.8")
     }
 
     override fun onLoad() {
